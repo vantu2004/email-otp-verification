db37b1d3b5633a44d35ad02a4541685e5e28d60c
vantu <tulevan526@gmail.com>
2025-04-16 07:33:35 +0700

    SCRUM-1: test commit code

--- src/main/java/com/pl/emailOtpVerification/EmailOtpVerificationApplication.java
+++ src/main/java/com/pl/emailOtpVerification/EmailOtpVerificationApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EmailOtpVerificationApplication {
 
+	// lê văn tú - test github-jira
+	
 	public static void main(String[] args) {
 		SpringApplication.run(EmailOtpVerificationApplication.class, args);
 	}
